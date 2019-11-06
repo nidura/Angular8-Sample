e0e956c297fa454578871760370bec6082dec264
nidura <niduraprageeth@gmail.com>
2019-11-06 16:53:04 +0530

    Slim loading bar added

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductGetComponent } from './product-get/product-get.component';
@@ -16,7 +17,8 @@ import { ProductGetComponent } from './product-get/product-get.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
