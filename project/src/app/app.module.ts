d931a721af6feed340feaabc97f0c43843e1e23e
nidura <niduraprageeth@gmail.com>
2019-11-06 16:02:36 +0530

    added three components

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductGetComponent } from './product-get/product-get.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductEditComponent,
+    ProductGetComponent
   ],
   imports: [
     BrowserModule,
